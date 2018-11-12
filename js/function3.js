c4b4544dccc751ea6191ca5a299230086d1944e3
Luca Crippa <luca.crippa88@gmail.com>
2018-11-12 16:34:27 +0100

    Added function3

--- js/function3.js
+++ js/function3.js
@@ -3,3 +3,10 @@
  * v 1.0
  * November 2018
  */
+
+msg.payload = {
+  chatId: msg.chatId,
+  type: "message",
+  content: "This is a " + msg.result.images[0].classifiers[0].classes[0].class
+};
+return msg;
