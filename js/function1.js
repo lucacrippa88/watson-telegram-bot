6569b3428eed547137b6687da60a7177e3e2b9bf
Luca Crippa <luca.crippa88@gmail.com>
2018-11-12 14:33:26 +0100

    Comments added

--- js/function1.js
+++ js/function1.js
@@ -1,3 +1,9 @@
+/* Function1.js
+ * Initialize Telegram bot messages
+ * v 1.0
+ * November 2018
+ */
+
 msg.chatId = msg.payload.chatId;
 msg.user = msg.payload.chatId;
 msg.payload = msg.payload.content;
--- js/function2.js
+++ js/function2.js
@@ -1,3 +1,9 @@
+/* Function2.js
+ * Prepare message to be sent to Telegram bot
+ * v 1.0
+ * November 2018
+ */
+
 var outputMsg = "";
 	if(msg.payload.output.text) {
     		for(i=0;i<msg.payload.output.text.length;i++){
