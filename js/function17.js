a5178abb94a9ff185825330fe72407dd3fd99c2f
Luca Crippa <luca.crippa88@gmail.com>
2018-12-08 18:57:16 +0100

    Added F17 and F18

---
+++ js/function17.js
@@ -0,0 +1,5 @@
+/* Function17.js
+ * Prepare deletion from DB
+ * v 1.0
+ * December 2018
+ */
---
+++ js/function18.js
@@ -0,0 +1,5 @@
+/* Function18.js
+ * Prepare answer for orders deleted
+ * v 1.0
+ * December 2018
+ */
